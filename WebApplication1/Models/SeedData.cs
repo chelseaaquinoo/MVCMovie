05ad60ac739916d26a892dcd629733fe376ba856
aquinoc2 <aquinoc2@D017LAB-06>
2018-12-06 09:40:38 -0500

    a

--- WebApplication1/Models/SeedData.cs
+++ WebApplication1/Models/SeedData.cs
@@ -43,6 +43,15 @@ namespace MvcMovie.Models
                          Price = 9.99M
                      },
 
+                     new Movie
+                     {
+                         Title = "When Harry Met Sally",
+                         ReleaseDate = DateTime.Parse("1989-1-11"),
+                         Genre = "Romantic Comedy",
+                         Rating = "R",
+                         Price = 7.99M
+                     },
+
                    new Movie
                    {
                        Title = "Rio Bravo",
